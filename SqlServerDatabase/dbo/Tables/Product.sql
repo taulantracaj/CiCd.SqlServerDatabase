5a45fe46e1d8027beb982f114029281abacc943e
taulant racaj <taulant.racaj@ecx.io>
2019-11-10 14:34:57 +0100

    Init

---
+++ SqlServerDatabase/dbo/Tables/Product.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [Id]    INT           NOT NULL,
+    [Name]  NVARCHAR (50) NULL,
+    [price] INT           NULL,
+    [enabled] BIT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
